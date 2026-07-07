9e221cf6fa8887ddf2b84977ead4f494ef56a9dd
raindrop <Gharantes@gmail.com>
2026-07-06 23:01:33 -0300

    FIlter by tag

--- frontend/src/app/modules/page-list-projects/form/form-list-projects.component.ts
+++ frontend/src/app/modules/page-list-projects/form/form-list-projects.component.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInput, MatLabel } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormField, MatPrefix } from '@angular/material/form-field';
 import { ConnectorListProjects } from '../connector/connector-list-projects';
-import { AddTagBtnComponent } from '@synergia-frontend/components';
-import { MatChip, MatChipSet, MatChipsModule } from '@angular/material/chips';
+import {
+  MatAutocomplete,
+  MatAutocompleteSelectedEvent,
+  MatAutocompleteTrigger,
+} from '@angular/material/autocomplete';
+import { MatOption } from '@angular/material/core';
 import { ITagModel } from '@synergia-frontend/interfaces';
+import { EntityTagResourceService } from '@synergia-frontend/api';
+import { SessionService } from '@synergia-frontend/services';
+import { TagDtoToModel } from '@synergia-frontend/mappers';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { debounceTime, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-form-list-projects',
@@ -25,23 +34,67 @@ import { ITagModel } from '@synergia-frontend/interfaces';
     FormsModule,
     MatLabel,
     ReactiveFormsModule,
-    AddTagBtnComponent,
-    MatChipSet,
-    MatChip,
-    MatChipsModule,
+    MatAutocomplete,
+    MatAutocompleteTrigger,
+    MatOption,
   ],
 })
-export class FormListProjectsComponent {
+export class FormListProjectsComponent implements OnInit {
   @Input() connector!: ConnectorListProjects;
 
-  protected saveTag($event: ITagModel) {
-    const control = this.connector.form.controls.tags;
-    if (control.value.some((v) => v.id === $event.id)) return;
-    control.setValue([...control.value, $event]);
+  private readonly tagService = inject(EntityTagResourceService);
+  private readonly sessionService = inject(SessionService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  public readonly tagSearchControl = new FormControl('', { nonNullable: true });
+  public readonly filteredTags$ = signal<ITagModel[]>([]);
+
+  ngOnInit(): void {
+    this.searchTags();
+    this.tagSearchControl.valueChanges
+      .pipe(
+        debounceTime(300),
+        tap(() => this.searchTags()),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
   }
 
-  protected removeTag(tag: ITagModel) {
+  private searchTags() {
+    const idTenant = this.sessionService.getTenantId();
+    if (idTenant == null) return;
+
+    this.tagService
+      .listTagsByTenant(idTenant, true, false, false, this.tagSearchControl.value)
+      .pipe(map((res) => res.map((v) => TagDtoToModel(v))))
+      .subscribe((res) => this.filteredTags$.set(res));
+  }
+
+  protected isSelected(tag: ITagModel): boolean {
+    return this.connector.form.controls.tags.value.some((v) => v.id === tag.id);
+  }
+
+  protected tagsPlaceholder(): string {
+    const tags = this.connector.form.controls.tags.value;
+    if (tags.length === 0) return 'Buscar tags...';
+    if (tags.length === 1) return tags[0].title;
+    return `${tags.length} tags selecionadas`;
+  }
+
+  protected selectTag(event: MatAutocompleteSelectedEvent, trigger: MatAutocompleteTrigger) {
+    const tag = event.option.value as ITagModel;
     const control = this.connector.form.controls.tags;
-    control.setValue(control.value.filter((v) => v.id !== tag.id));
+    const alreadySelected = control.value.some((v) => v.id === tag.id);
+
+    control.setValue(
+      alreadySelected
+        ? control.value.filter((v) => v.id !== tag.id)
+        : [...control.value, tag]
+    );
+
+    this.tagSearchControl.setValue('');
+    this.searchTags();
+
+    setTimeout(() => trigger.openPanel());
   }
 }
