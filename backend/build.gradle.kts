cfe3096c59bc744372bf60adba171783fb5e4080
Gharantes <work.gharantes@gmail.com>
2026-09-10 16:58:22 -0300

    Atualiza o Kotest para a 6.2.4

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
 	}
 	/* Test Dependencies */
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	val kotestVersion = "5.9.1"
+	val kotestVersion = "6.2.4"
 	testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
 	testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 	// Use the Kotlin JUnit 5 integration.
