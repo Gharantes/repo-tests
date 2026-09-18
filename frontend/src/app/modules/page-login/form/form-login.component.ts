9910e3abbfbfc469d019d1e33081361bd39638a5
raindrop <Gharantes@gmail.com>
2026-09-18 12:56:49 -0300

    Login page

--- frontend/src/app/modules/page-login/form/form-login.component.ts
+++ frontend/src/app/modules/page-login/form/form-login.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, signal } from '@angular/core';
 import { ConnectorLogin } from '../connector/connector-login';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatOption, MatRippleModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatAutocomplete, MatAutocompleteTrigger } from '@angular/material/autocomplete';
 
 import { ITenantModel } from '@synergia-frontend/interfaces';
@@ -22,6 +23,7 @@ import { ITenantModel } from '@synergia-frontend/interfaces';
     MatInputModule,
     MatRippleModule,
     MatButtonModule,
+    MatIconModule,
     MatAutocomplete,
     MatAutocompleteTrigger,
     MatOption
@@ -29,6 +31,7 @@ import { ITenantModel } from '@synergia-frontend/interfaces';
 })
 export class FormLoginComponent {
   @Input() public connector!: ConnectorLogin;
+  public readonly hidePassword = signal(true);
   displayTenant: ((value: ITenantModel|string|null) => string) = (res) => {
     if (res == null) {
       return '';
--- frontend/src/app/modules/page-login/view/view-login.component.ts
+++ frontend/src/app/modules/page-login/view/view-login.component.ts
@@ -6,6 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { ITenantModel } from '@synergia-frontend/interfaces';
 import { MatRippleModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { FormLoginComponent } from '../form/form-login.component';
 
 @Component({
@@ -21,6 +22,7 @@ import { FormLoginComponent } from '../form/form-login.component';
     MatInputModule,
     MatRippleModule,
     MatButtonModule,
+    MatIconModule,
     FormLoginComponent,
   ],
 })
